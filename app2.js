b7e2c3281ae20aaef1fa89f10787706029883e43
Tanvi Pandey <61287317+tannypandey@users.noreply.github.com>
2020-07-06 16:21:45 +0530

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,7 @@
+var answer = prompt('Are you from UK, Spain, India, Australia, Chile?')
+if (answer.toLowerCase() === 'yes'){
+    alert("Great! We have your country here");
+}
+else{
+    alert("It's okay you can discover new music here");
+}
